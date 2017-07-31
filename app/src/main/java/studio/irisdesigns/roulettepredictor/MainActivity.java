3f5cd213f4959ba5d6f39e0498e29493bb065ed2
viditkothari <kothari.vidit@gmail.com>
2017-07-31 22:23:50 +0530

    c2: 1) Implemented 1st draft for the UI with 4 screens: a) spin / dice icon b) predict / calc icon c) statistics / stats icon d) method / gear icon
    2) Add icons for tabs

--- app/src/main/java/studio/irisdesigns/roulettepredictor/MainActivity.java
+++ app/src/main/java/studio/irisdesigns/roulettepredictor/MainActivity.java
@@ -1,13 +1,81 @@
 package studio.irisdesigns.roulettepredictor;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        Log.i("tabl_0", (tabLayout.getTabAt(0)).toString());
+        tabLayout.getTabAt(0).setIcon(R.drawable.dice);
+        tabLayout.getTabAt(1).setIcon(R.drawable.predict);
+        tabLayout.getTabAt(2).setIcon(R.drawable.stats);
+        tabLayout.getTabAt(3).setIcon(R.drawable.gear);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new RouletteStageFragment(), "Stage");
+        adapter.addFragment(new PredictorFragment(), "Predict");
+        adapter.addFragment(new StatisticsFragment(), "Stats");
+        adapter.addFragment(new MethodologyFragment(), "Method");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            /* Un-comment the statement below to display Page title on the Tabs / headers */
+            // return mFragmentTitleList.get(position);
+            return null;
+        }
     }
 }
